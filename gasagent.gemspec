50562103d9789bb066ca15298a3a6240bc302e50
Jorja Hung <jorjahung@gmail.com>
2014-09-10 16:08:14 +0100

    can now download

---
+++ gasagent.gemspec
@@ -0,0 +1,29 @@
+# coding: utf-8
+lib = File.expand_path('../lib', __FILE__)
+$LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
+require 'gasagent/version'
+
+Gem::Specification.new do |spec|
+  spec.name          = "gasagent"
+  spec.version       = "1.0.0"
+  spec.authors       = ["Jorja Hung"]
+  spec.email         = ["jorjahung@gmail.com"]
+  spec.summary       = %q{A gem for importing, exporting, and creating new Google Apps Scripts from/to your Google Drive}
+  spec.description   = %q{A gem for importing, exporting, and creating new Google Apps Scripts from/to your Google Drive}
+  spec.homepage      = ""
+  spec.license       = "MIT"
+
+  spec.files         = `git ls-files -z`.split("\x0")
+  spec.executables   = spec.files.grep(%r{^bin/}) { |f| File.basename(f) }
+  spec.test_files    = spec.files.grep(%r{^(test|spec|features)/})
+  spec.require_paths = ["lib"]
+
+  spec.add_development_dependency "bundler", "~> 1.6"
+  spec.add_development_dependency "rake"
+  spec.add_development_dependency "rspec"
+  spec.add_development_dependency "rspec-mocks"
+  spec.add_runtime_dependency  'google-api-client', '>= 0.6'
+  spec.add_runtime_dependency  'launchy', '>= 2.1.1'
+  spec.add_runtime_dependency  'json'
+  spec.add_runtime_dependency  'httparty'
+end
---
+++ lib/gasagent.rb
@@ -0,0 +1,78 @@
+require 'list'
+require 'fetch'
+require 'fileutils'
+require 'json'
+require 'launchy'
+require 'httparty'
+require 'google/api_client'
+require 'google/api_client/client_secrets'
+require 'google/api_client/auth/file_storage'
+require 'google/api_client/auth/installed_app'
+
+class GASAgent
+	include List, Fetch
+	attr_accessor :client, :drive, :all_projects
+
+	API_KEY = ENV['GAS_API_KEY']
+	API_VERSION = 'v2'
+	CACHED_API_FILE = "drive-#{API_VERSION}.cache"
+	CREDENTIAL_STORE_FILE = "#{$0}-oauth2.json"
+	DEFAULT_PATH = Dir.pwd
+	APPS_SCRIPTS_FILES_STORE = "imported_apps_script_data.json"
+
+  def initialize
+  	@client, @drive = setup
+  	@all_files = retrieve_drive_files_list(@client, @drive)
+  	@all_projects = retrieve_apps_script(@all_files)
+  	@path = DEFAULT_PATH
+	end
+
+	def setup
+	  log_file = File.open('drive.log', 'a+')
+	  log_file.sync = true
+	  logger = Logger.new(log_file)
+	  logger.level = Logger::DEBUG
+
+	  client = Google::APIClient.new(:application_name => 'GAS Agent',
+	      :application_version => '1.0.0')
+
+	  # FileStorage stores auth credentials in a file, so they survive multiple runs
+	  # of the application. This avoids prompting the user for authorization every
+	  # time the access token expires, by remembering the refresh token.
+	  # Note: FileStorage is not suitable for multi-user applications.
+	  file_storage = Google::APIClient::FileStorage.new(CREDENTIAL_STORE_FILE)
+	  if file_storage.authorization.nil?
+	    client_secrets = Google::APIClient::ClientSecrets.load
+	    # The InstalledAppFlow is a helper class to handle the OAuth 2.0 installed
+	    # application flow, which ties in with FileStorage to store credentials
+	    # between runs.
+	    flow = Google::APIClient::InstalledAppFlow.new(
+	      :client_id => client_secrets.client_id,
+	      :client_secret => client_secrets.client_secret,
+	      :scope => ['https://www.googleapis.com/auth/drive', 'https://www.googleapis.com/auth/drive.scripts']
+	    )
+	    client.authorization = flow.authorize(file_storage)
+	  else
+	    client.authorization = file_storage.authorization
+	  end
+
+	  drive = nil
+	  # Load cached discovered API, if it exists. This prevents retrieving the
+	  # discovery document on every run, saving a round-trip to API servers.
+	  if File.exists? CACHED_API_FILE
+	    File.open(CACHED_API_FILE) do |file|
+	      drive = Marshal.load(file)
+	    end
+	  else
+	    drive = client.discovered_api('drive', API_VERSION)
+	    File.open(CACHED_API_FILE, 'w') do |file|
+	      Marshal.dump(drive, file)
+	    end
+	  end
+
+	  return client, drive
+	end
+
+	
+
+end
